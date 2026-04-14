624998fa0d1dff7c2ad7ea932a239732764e83e2
aman Umarvaishya <amanumarvaishya9653021436@gmail.com>
2026-04-14 00:15:14 -0800

    bi

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { FaCode, FaCogs, FaCss3, FaFigma, FaHtml5, FaJs, FaReact } from "react-icons/fa";
-import { SiExpress, SiNodedotjs, SiJava, SiMysql } from "react-icons/si";
-import { FaGoogle } from "react-icons/fa";
+import { FaCode, FaCogs, FaCss3, FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import { SiExpress, SiNodedotjs,  SiMysql } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
-import { RiNetflixFill } from "react-icons/ri";
-import { FaAmazon } from "react-icons/fa";
 const Experience = () => {
   return (
     <div id="Experience" className="p-10 md:p-24">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Hand-On-Experience</h1>
       <div className=" flex justify-center ">
         <div className="flex flex-wrap md:w-2/5 gap-8 md:p-12 py-10">
-         <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-            <SiJava color="#f89820" size={50} />
-          </span>
+         
            <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
-            <SiMysql color="#00758E" size={50} />
+            <SiMysql color="#00758F" size={50} />
           </span>
 
           <span className="p-3 bg-zinc-950 flex items-center rounded-2xl">
